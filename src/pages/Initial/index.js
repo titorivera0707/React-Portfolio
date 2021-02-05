59b97941624e07a2079ac79be510ab5cce50ffb7
Tito Rivera <titorivera0707@gmail.com>
2021-02-05 15:57:19 -0800

    Updated initial page to work

--- src/pages/Initial/index.js
+++ src/pages/Initial/index.js
@@ -1,15 +1,18 @@
 import React, {Image} from "react"
+import { Link } from "react-router-dom";
 import Particles from "react-particles-js"
+import "./style.css"
 
 function Initial() {
 
     function clickButton() {
-
+      console.log("Hello")
     }
 
     return(
+      <div>
         <Particles
-        style={{ position: "absolute" }}
+        style={{ position: "fixed" }}
         params={{
           particles: {
             color: {
@@ -28,9 +31,17 @@ function Initial() {
             }
           }
         }}
-        >
-Hello
-        </Particles>
+        />
+          <div className="newDiv">
+            <h1 className="newTop">Hello World</h1>
+            <button className="buttonInit">
+            <Link className="navbar-brand" to="/home">
+          Home
+        </Link>
+            </button>
+          </div>
+        </div>
+        
     )
 }
 
