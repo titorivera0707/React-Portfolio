784f0ba442349cb15cd00d76e49e32f5e4eb2824
Tito Rivera <titorivera0707@gmail.com>
2021-02-13 20:28:15 -0800

    Updates to footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,10 @@ import "./style.css"
 function Footer() {
   return (
       <div className="footer">
-    <footer>
+    <div>
       <hr />
       <p><a href="https://www.linkedin.com/in/tito-rivera-3477981b5" className="fa fa-linkedin-square"></a> <a href="https://github.com/titorivera0707" class="git fa fa-github-square"></a></p>
-    </footer>
+    </div>
     </div>
   );
 }
