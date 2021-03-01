a477937fcf5c44cb9948c25c686ddf802894f964
Tito Rivera <titorivera0707@gmail.com>
2021-02-28 21:08:00 -0800

    Updated projects page and added new project

--- src/pages/ProjectsPage/index.js
+++ src/pages/ProjectsPage/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom";
 import "./style.css"
 import First from "../../components/images/calendar.png"
 import Second from "../../components/images/weather.png"
@@ -12,28 +13,80 @@ import Carousell from "../../components/Carousel"
 import Early1 from "../../components/images/Early1.png"
 import Early2 from "../../components/images/Early2.png"
 import Early3 from "../../components/images/Early3.png"
+import Weather1 from "../../components/images/Weather1.png"
+import Weather2 from "../../components/images/Weather2.png"
+import Weather3 from "../../components/images/Weather3.png"
 
 function Portfolio() {
+
+    function gitEarly() {
+        window.location="https://github.com/titorivera0707/project3po"
+    }
+    function siteEarly() {
+        window.location="https://stormy-bayou-72071.herokuapp.com/"
+    }
+    function gitWeather() {
+        window.location="https://github.com/titorivera0707/Weather-Dashboard"
+    }
+    function siteWeather() {
+        window.location="https://titorivera0707.github.io/Weather-Dashboard/"
+    }
+
     return(
         <div>
             <Nav />
-            <div className="picture">
-            <h1 className="center">Project Early Bird</h1>
-            <p className="center">Created using: React, CSS, Bootstrap</p>
-            <Carousell FirstP={Early1} D1="How does this work" P1="It works like this I think"
-            SecondP={Early2}
-            ThirdP={Early3}/>
-            <div class="row">
-            <div className="col-8">
-            <h4>Project early bird was created for those of us who are always made late by the little things in life. We always have big things saved in our calendar about things that will be happening during our week or even month, but we always forget to take into account how much time we need to make it on time for those certain events. Most calendars don't take into account things ike brushing your teeth, getting dressed, eating breakfast. All those few minutes wasted doing necessary time is time that we sometimes don't take into account and thus we are made late. Early bird solves this issue by allowing the user to input things in their daily calendar by 1 or 5 minute increments rather than by hour as most daily planners do.</h4>
-            </div>
-            <div className="col-4">
-                
-            </div>
 
+            <div className="picture text">
+                <div className="header">
+                    <h1 className="center">Project Early Bird</h1>
+                </div>
+                <p className="center">Created using: React, CSS, Bootstrap</p>
+                <Carousell FirstP={Early1}
+                SecondP={Early2}
+                ThirdP={Early3}/>
+                <br/>
+                <div class="row text">
+                    <div className="col-8">
+                        <h4>Project early bird was created for those of us who are always made late by the little things in life. We always have big things saved in our calendar about things that will be happening during our week or even month, but we always forget to take into account how much time we need to make it on time for those certain events. Most calendars don't take into account things ike brushing your teeth, getting dressed, eating breakfast. All those few minutes wasted doing necessary time is time that we sometimes don't take into account and thus we are made late. Early bird solves this issue by allowing the user to input things in their daily calendar by 1 or 5 minute increments rather than by hour as most daily planners do.</h4>
+                    </div>
+                    <div className="col-4 center">
+                        <button className="buttonProj" target="_blank" onClick={gitEarly}>
+                            Github Repository
+                        </button>
+                        <br/>
+                        <button className="buttonProj" target="_blank" onClick={siteEarly}>
+                            Project Website
+                        </button>
+                    </div>
+                </div>
             </div>
+            <br/>
+            <hr/>
+            <br/>
+
+            <div className="picture text">
+                <div className="header">
+                    <h1 className="center">Simple Weather App</h1>
+                </div>
+                <p className="center">Created using: Javascript, CSS, HTML</p>
+                <Carousell FirstP={Weather1}
+                SecondP={Weather2}
+                ThirdP={Weather3}/>
+                <div class="row">
+                    <div className="col-8">
+                        <h4></h4>
+                    </div>
+                    <div className="col-4 center">
+                        <button className="buttonProj" target="_blank" onClick={gitWeather}>
+                            Github Repository
+                        </button>
+                        <br/>
+                        <button className="buttonProj" target="_blank" onClick={siteWeather}>
+                            Project Website
+                        </button>
+                    </div>
+                </div>
             </div>
-            <h1>Project Two</h1>
 
             <h1>Project Three</h1>
 
